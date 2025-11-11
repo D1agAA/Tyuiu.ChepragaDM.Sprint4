5665a90f7ee67870617d3570e156f7dc96f25e4b
DiagAA 69 <dima.prosto.2019@inbox.ru>
2025-11-11 20:11:05 +0500

    Task7

---
+++ Tyuiu.ChepragaDM.Sprint4.Task7.V24.Lib/DataService.cs
@@ -0,0 +1,34 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.ChepragaDM.Sprint4.Task7.V24.Lib
+{
+    public class DataService : ISprint4Task7V24
+    {
+        public int Calculate(int n, int m, string value)
+        {
+            int[,] a = new int[n, m];
+
+            int index = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    a[i,j] = int.Parse(value.Substring(i * m + j, 1));
+                }
+            }
+            int res = 1;
+            for(int i = 0;i < n; i++)
+            {
+                for(int j = 0;  j < m; j++)
+                {
+                    if (a[i, j] % 2 == 0)
+                    {
+                        res *= a[i, j];
+                    }
+
+                }
+            }
+            return res;
+        }
+    }
+}
